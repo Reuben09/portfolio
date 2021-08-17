6ee12e67f833dfc13eb74872953c59298b2f9ad8
Reuben09 <66923560+Reuben09@users.noreply.github.com>
2021-08-17 18:06:17 +0100

    Add files via upload

---
+++ my-portfolio/script.js
@@ -0,0 +1,8 @@
+// get elements
+var toggleButton = document.querySelector('.toggle-button');
+var navLinks = document.querySelector('.navlinks');
+
+ toggleButton.addEventListener('click', function(){
+     navLinks.classList.toggle('display-nav');
+ })
+
